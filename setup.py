5feabd745ca72d115d4d6519fa45ea6c7a3234d2
Noah Davis <adoyle@superchunk.(none)>
2013-08-25 11:45:55 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+__version__ = '0.3'
+
+setup( 
+    name='aamva2011',
+    version=__version__, 
+    py_modules=['aamva2011'], 
+    author = 'Noah Davis',
+    author_email = 'noah@tty72.com',
+    description = 'Simple AAMVA 2011 data parser'
+    )
